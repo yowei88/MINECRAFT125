fe0e6a88c8f8254ef7ef6b4bd6db1625b5171488
yowei88 <73010491+yowei88@users.noreply.github.com>
2020-12-05 12:01:46 +0800

    Add files via upload

---
+++ 125-1.py
@@ -0,0 +1,9 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+while True:
+    t=mc.events.pollBlockHits()
+    if len(t)>0:
+        hit=t[0]
+        x,y,z=hit.pos.x,hit.pos.y,hit.pos.z
+        b=mc.getBlock(x,y,z)
+        mc.postToChat("youget"+str(b))
